22dfef146b796e8712177481929ccbc510316772
Michael Stella <michael573114@gmail.com>
2011-04-27 18:39:22 -0400

    Added a tracking event object, and an array of said events to the result object.  This way client software will have a shipper-independent way to query the event log for a package.

--- packagetrack/data.py
+++ packagetrack/data.py
@@ -3,6 +3,8 @@ class TrackingInfo(object):
     """Generic tracking information object returned by a tracking request"""
 
     def __init__(self, delivery_date, status, last_update, location=None, delivery_detail=None):
+        self.events = []
+
         self.delivery_date = delivery_date
         self.status = status
         self.last_update = last_update
@@ -34,3 +36,23 @@ class TrackingInfo(object):
                             self.location,
                         )
                     )
+
+
+    def addEvent(self, date, location, detail):
+        e = TrackingEvent(date, location, detail)
+        self.events.append(e)
+        return e
+
+
+class TrackingEvent(object):
+    """An individual tracking event, i.e. a status change"""
+
+    def __init__(self, date, location, detail):
+        self.date = date
+        self.location = location
+        self.detail = detail
+
+    def __repr__(self):
+        return ('<TrackingEvent(date=%r, location=%r, detail=%r)>' %
+                    (self.date, self.location, self.detail))
+
