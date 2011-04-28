f338459b101ee2ae358c23b952cc7fb9ecfdd9dc
Michael Stella <michael573114@gmail.com>
2011-04-28 14:38:35 -0400

    Updated the UPS tracker to provide the full tracking history (creating TrackingEvent objects).
    Handling a special case when a 'UPS BASIC' package was delivered to the local
    P.O.
    
    Changed private method names to underscore prefix for clarity.
    
    Validating tracking number before making HTTP request.

--- packagetrack/service/ups_interface.py
+++ packagetrack/service/ups_interface.py
@@ -4,7 +4,7 @@ from datetime import datetime, date, time
 import packagetrack
 from ..xml_dict import dict_to_xml, xml_to_dict
 from ..data import TrackingInfo
-from ..service import BaseInterface
+from ..service import BaseInterface, TrackFailed, InvalidTrackingNumber
 
 class UPSInterface(BaseInterface):
     api_url = 'https://wwwcie.ups.com/ups.app/xml/Track'
@@ -35,32 +35,44 @@ class UPSInterface(BaseInterface):
         check = ((total - (total % 10) + 10) - total) % 10
         return (check == int(tracking_number[-1:]))
 
-    def build_access_request(self):
+    def _build_access_request(self):
         config = packagetrack.config
-        d = {'AccessRequest':
-             {'AccessLicenseNumber': config.get('UPS', 'license_number'),
-              'UserId': config.get('UPS', 'user_id'),
-              'Password': config.get('UPS', 'password')}}
+        d = {
+            'AccessRequest': {
+                'AccessLicenseNumber': config.get('UPS', 'license_number'),
+                'UserId': config.get('UPS', 'user_id'),
+                'Password': config.get('UPS', 'password')
+            }
+        }
         return dict_to_xml(d, self.attrs)
 
-    def build_track_request(self, tracking_number):
-        req = {'TransactionReference': {'RequestAction': 'Track'}}
-        d = {'TrackRequest': {'Request': req,
-                              'TrackingNumber': tracking_number}}
+    def _build_track_request(self, tracking_number):
+        req = {
+            'TransactionReference': {
+                'RequestAction': 'Track',
+            },
+            'RequestOption': '1',
+        }
+        d = {
+            'TrackRequest': {
+                'Request': req,
+                'TrackingNumber': tracking_number
+            }
+        }
         return dict_to_xml(d)
 
-    def build_request(self, tracking_number):
-        return (self.build_access_request() +
-                self.build_track_request(tracking_number))
+    def _build_request(self, tracking_number):
+        return (self._build_access_request() +
+                self._build_track_request(tracking_number))
 
-    def send_request(self, tracking_number):
-        body = self.build_request(tracking_number)
+    def _send_request(self, tracking_number):
+        body = self._build_request(tracking_number)
         webf = urllib.urlopen(self.api_url, body)
         resp = webf.read()
         webf.close()
         return resp
 
-    def parse_response(self, raw):
+    def _parse_response(self, raw):
         root = xml_to_dict(raw)['TrackResponse']
 
         response = root['Response']
@@ -68,46 +80,107 @@ class UPSInterface(BaseInterface):
         status_description = response['ResponseStatusDescription']
         # Check status code?
 
+        # we need the service code, some things are treated differently
+        service_code = root['Shipment']['Service']['Code']
+        service_description = 'UPS %s' % root['Shipment']['Service']['Description']
+
         package = root['Shipment']['Package']
-        activity = package['Activity']
+
+        # make activites a list if it's not already
+        if type(package['Activity']) != list:
+            package['Activity'] = [package['Activity']]
+
+        # this is the last activity, the one we get status info from
+        activity = package['Activity'][0]
+
+        # here's the status code, inside the Activity block
         status = activity['Status']['StatusType']['Description']
         status_code = activity['Status']['StatusType']['Code']
-
 
         last_update_date = datetime.strptime(activity['Date'], "%Y%m%d").date()
         last_update_time = datetime.strptime(activity['Time'], "%H%M%S").time()
         last_update = datetime.combine(last_update_date, last_update_time)
 
-        # the last known location is interesting
-        loc = activity['ActivityLocation']['Address']
-        last_location = ','.join((loc['City'],
-                                  loc['StateProvinceCode'],
-                                  loc['CountryCode']))
-
-        # Delivery date is the last_update if delivered, otherwise
-        # the estimated delivery date
-        if status_code == 'D':
+        # 031 = BASIC service, delivered to local P.O., so we use the
+        # ShipTo address to get the city, state, country
+        # this may never be considered D=Delivered, best we can do
+        # is just report that the local P.O. got it.
+        #
+        # note this also has no SDD, so we just use the last update
+        if service_code == '031':
+            loc = root['Shipment']['ShipTo']['Address']
+            last_location = ','.join((loc['City'],
+                                      loc['StateProvinceCode'],
+                                      loc['CountryCode']))
             delivery_date = last_update
 
         else:
-            delivery_date = datetime.strptime(
-                root['Shipment']['ScheduledDeliveryDate'], "%Y%m%d")
+            # the last known location is interesting
+            loc = activity['ActivityLocation']['Address']
+            last_location = ','.join((loc['City'],
+                                      loc['StateProvinceCode'],
+                                      loc['CountryCode']))
 
-        delivery_detail = None
+            # Delivery date is the last_update if delivered, otherwise
+            # the estimated delivery date
+            if status_code == 'D':
+                delivery_date = last_update
+
+            elif 'RescheduledDeliveryDate' in package:
+                delivery_date = datetime.strptime(
+                    package['RescheduledDeliveryDate'], "%Y%m%d")
+            else:
+                delivery_date = datetime.strptime(
+                    root['Shipment']['ScheduledDeliveryDate'], "%Y%m%d")
+
+
+        # Delivery detail may not always be available either
         if 'Description' in activity['ActivityLocation']:
             delivery_detail = activity['ActivityLocation']['Description']
+        else:
+            delivery_detail = status
 
-        return TrackingInfo(last_update     = last_update,
+        trackinfo = TrackingInfo(
+                            last_update     = last_update,
                             delivery_date   = delivery_date,
                             status          = status,
                             location        = last_location,
                             delivery_detail = delivery_detail,
+                            service         = service_description,
                             )
+
+        # add a single event, UPS doesn't seem to support multiple?
+
+        for e in package['Activity']:
+            # the last known location is interesting
+            loc = e['ActivityLocation']['Address']
+            location=None
+            if 'City' in loc:
+                location = ','.join((loc['City'],
+                                     loc['StateProvinceCode'],
+                                     loc['CountryCode']))
+
+            edate = datetime.strptime(e['Date'], "%Y%m%d").date()
+            etime = datetime.strptime(e['Time'], "%H%M%S").time()
+            timestamp = datetime.combine(edate, etime)
+            trackinfo.addEvent(
+                location = location,
+                detail = e['Status']['StatusType']['Description'],
+                date = timestamp,
+            )
+
+
+        return trackinfo
+
 
     def track(self, tracking_number):
         "Track a UPS package by number. Returns just a delivery date."
-        resp = self.send_request(tracking_number)
-        return self.parse_response(resp)
+
+        if not self.validate(tracking_number):
+            raise InvalidTrackingNumber()
+
+        resp = self._send_request(tracking_number)
+        return self._parse_response(resp)
 
     def url(self, tracking_number):
         "Return a tracking info detail URL by number."
